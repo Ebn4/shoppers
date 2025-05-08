f1bf16a31dac94eb030301313869835bd6f149a8
Exauce Bodi <exaucebodi4@gmail.com>
2025-05-08 22:15:01 +0200

    Creation d'un splash screen qui disparait après 2 sécondes

--- lib/pages/widgets/splashScreen.dart
+++ lib/pages/widgets/splashScreen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:shoppers/pages/login/loginPage.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -8,23 +11,35 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    // Simulate a network request or some initialization
-    Future.delayed(Duration(seconds: 3), () {
-      Navigator.pushReplacementNamed(context, '/home');
+    // Simulate a delay for 2 seconds
+    // before navigating to the login page
+    Timer(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => Loginpage()),
+      );
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              
-            ],
-          ),
+      backgroundColor: Colors.yellow,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Welcome to Shoppers", style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+            // SizedBox(height: 10),
+            Image.asset(
+              'assets/images/splash.png',
+              width: 200,
+              height: 700,
+            ),
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+            ),
+          ],
         ),
       )
     );
